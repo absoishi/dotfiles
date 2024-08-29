95336035484e411e681a30ca6f6d6b21d2578982
absoishi <absoishi@gmail.com>
2024-08-29 23:31:04 +0900

    modified lsp.lua

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -30,7 +30,17 @@ return{
       'nvim-treesitter/nvim-treesitter',
       'nvim-tree/nvim-web-devicons',
     },
-		config = true,
+    config = function()
+      require('lspsaga').setup({})
+      vim.keymap.set('n', 'K', '<cmd>Lspsaga hover_doc<CR>')
+      vim.keymap.set('n', 'gr', '<cmd>Lspsaga lsp_finder<CR>')
+      vim.keymap.set('n', 'gd', '<cmd>Lspsaga peek_definition<CR>')
+      vim.keymap.set('n', 'ga', '<cmd>Lspsaga code_action<CR>')
+      vim.keymap.set('n', 'gn', '<cmd>Lspsaga rename<CR>')
+      vim.keymap.set('n', 'ge', '<cmd>Lspsaga show_line_diagnostics<CR>')
+      vim.keymap.set('n', '[e', '<cmd>Lspsaga diagnostic_jump_next<CR>')
+      vim.keymap.set('n', ']e', '<cmd>Lspsaga diagnostic_jump_prev<CR>')
+    end,
 	},
   {
     'folke/lsp-colors.nvim',
@@ -39,7 +49,40 @@ return{
   {
     'folke/trouble.nvim',
     dependencies = {'kyazdani42/nvim-web-devicons'},
-    config = true,
+    opts = {},
+    cmd = "Trouble",
+    keys = {
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
   },
   {
     'j-hui/fidget.nvim',
